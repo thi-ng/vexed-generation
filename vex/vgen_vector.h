2d4c95392abd6d197c09542d3c645863d0e0bbf1
Karsten Schmidt <k@postspectacular.com>
2020-06-06 10:25:36 +0100

    feat: add vg_swizzle, vg_vec* ctors

--- vex/vgen_vector.h
+++ vex/vgen_vector.h
@@ -10,10 +10,26 @@
 //   \___  / \___  >___|  /\___  >__|  (____  /__| |__|\____/|___|  /
 //  /_____/      \/     \/     \/           \/                    \/
 //
-//  (c) 2016 Karsten Schmidt // ASL 2.0 licensed
+//  (c) 2016 - 2020 Karsten Schmidt // ASL 2.0 licensed
 
 #ifndef __vgen_vector_h__
 #define __vgen_vector_h__
+
+vector2 vg_vec2(float x, y) {
+    return set(x, y);
+}
+
+vector2 vg_vec2(vector3 v) {
+    return set(v.x, v.y);
+}
+
+vector2 vg_vec2(vector4 v) {
+    return set(v.x, v.y);
+}
+
+vector vg_vec3(vector2 v) {
+    return set(v.x, v.y, 0);
+}
 
 vector vg_vec3(vector2 xy; float z) {
     return set(xy.x, xy.y, z);
@@ -29,6 +45,42 @@ vector4 vg_vec4(vector2 xy, zw) {
 
 vector4 vg_vec4(vector xyz; float w) {
     return set(xyz.x, xyz.y, xyz.z, w);
+}
+
+vector2 vg_swizzle(vector2 v; int x, y) {
+    return set(v[x], v[y]);
+}
+
+vector vg_swizzle(vector2 v; int x, y, z) {
+    return set(v[x], v[y], v[z]);
+}
+
+vector4 vg_swizzle(vector2 v; int x, y, z, w) {
+    return set(v[x], v[y], v[z], v[w]);
+}
+
+vector2 vg_swizzle(vector v; int x, y) {
+    return set(v[x], v[y]);
+}
+
+vector vg_swizzle(vector v; int x, y, z) {
+    return set(v[x], v[y], v[z]);
+}
+
+vector4 vg_swizzle(vector v; int x, y, z, w) {
+    return set(v[x], v[y], v[z], v[w]);
+}
+
+vector2 vg_swizzle(vector4 v; int x, y) {
+    return set(v[x], v[y]);
+}
+
+vector vg_swizzle(vector4 v; int x, y, z) {
+    return set(v[x], v[y], v[z]);
+}
+
+vector4 vg_swizzle(vector4 v; int x, y, z, w) {
+    return set(v[x], v[y], v[z], v[w]);
 }
 
 #endif
